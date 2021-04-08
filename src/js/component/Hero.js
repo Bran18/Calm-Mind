5b69a918aadac16eafe781e4e91015c96cca887a
Brandon Fernández <andreyfdez18@gmail.com>
2021-04-08 23:56:43 +0000

    m

---
+++ src/js/component/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Hero = () => {
+	return (
+		<div className="container">
+			<h1>ADVENTURE AWAITS</h1>
+			<p>What are you waiting for?</p>
+		</div>
+	);
+};
+
+export default Hero;
--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -2,9 +2,17 @@ import React, { Component } from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+		<div className="container">
+			<ul className="list-unstyled list-inline text-center py-2">
+				<li className="list-inline-item">
+					<h5 className="mb-1">Register for free</h5>
+				</li>
+				<li className="list-inline-item">
+					<button className="btn btn-outline-primary btn-rounded">Sign up!</button>
+				</li>
+			</ul>
+		</div>
+
+		<div className="footer-copyright text-center py-3">© 2021 Copyright:</div>
 	</footer>
 );
